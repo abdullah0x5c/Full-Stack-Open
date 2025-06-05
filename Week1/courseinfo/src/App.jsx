70b82b125bbf8ca4f3ef1528a8244811f7db3e48
abdullah0x5c <1abdullahraja1@gmail.com>
2025-06-05 16:23:14 +0500

    Part 1.3

--- Week1/courseinfo/src/App.jsx
+++ Week1/courseinfo/src/App.jsx
@@ -3,15 +3,21 @@ import Content from './Content'
 import Total from './Total'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const parts = [part1, part2, part3]
-  const ex = [exercises1, exercises2, exercises3]
+    const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const parts = [part1.name, part2.name, part3.name]
+  const ex = [part1.exercises, part2.exercises, part3.exercises]
 
 
   return (
