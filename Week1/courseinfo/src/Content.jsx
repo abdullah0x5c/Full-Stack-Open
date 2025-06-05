f64f9cf5b69b413218f5acebab9d5e4b93ed6403
abdullah0x5c <1abdullahraja1@gmail.com>
2025-06-05 16:29:13 +0500

    Part 1.4

--- Week1/courseinfo/src/Content.jsx
+++ Week1/courseinfo/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from './Part'
 const Content = (props) => {
     return (
         <>
-            <Part part={props.part[0]} exercise={props.exercises[0]} />
-            <Part part={props.part[1]} exercise={props.exercises[1]} />
-            <Part part={props.part[2]} exercise={props.exercises[2]} />
+            <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+            <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+            <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
         </>
     )
 }
