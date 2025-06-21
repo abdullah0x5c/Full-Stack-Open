ef064be97d10fb1e37934b293b31e4980f61c560
abdullah0x5c <1abdullahraja1@gmail.com>
2025-06-21 14:28:40 +0500

    1.9: unicafe step 4

--- Week1/unicafe/src/App.jsx
+++ Week1/unicafe/src/App.jsx
@@ -17,16 +17,25 @@ const Statistics = (props) => {
   const avg = (good*1+bad*-1)/all
   const psv = ((good)/all)*100
 
-  return (
-    <>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {all}</p>
-    <p>average {avg}</p>
-    <p>positive {psv}%</p>
-    </>
+  if(all == 0){
+    return(
+      <p>No Feedback Given</p>
+    )
+  }
+  else {
+    return (
+      <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {avg}</p>
+        <p>positive {psv}%</p>
+      </>
   )
+  }
+
+
 }
 
 
