ed45109da9bdafb64959b70ebbc774325fde08ea
abdullah0x5c <1abdullahraja1@gmail.com>
2025-06-25 18:10:02 +0500

    2.7: The Phonebook Step 2

--- Week2/phonebook/src/App.jsx
+++ Week2/phonebook/src/App.jsx
@@ -11,13 +11,25 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const notExists = () => {
+    console.log(persons)
+    const finds = persons.find((person) => person.name == newName)
+    return !finds
+  }
+
   const submit = (event) => {
     event.preventDefault()
-    setPersons([...persons,{name: newName}])
+    if(notExists()){
+      setPersons([...persons,{name: newName}])
+    }
+    else{
+      alert(`${newName} is already added to phonebook`)
+    }
   }
 
   const names = persons.map((person, index) => <li key = {index} > {person.name}</li>)
 
+  
   return (
     <div>
       <h2>Phonebook</h2>
