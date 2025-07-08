f5171a7051f090024c05bebb8670a1a0368b466f
abdullah0x5c <1abdullahraja1@gmail.com>
2025-07-08 14:01:25 +0500

    2.17*: Phonebook step 12

--- Week2/phonebook/src/App.jsx
+++ Week2/phonebook/src/App.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import Person from './Person.jsx'
 import Filter from './Filter.jsx'
 import PersonForm from './PersonForm.jsx'
+import Error from './Errorr.jsx'
 import Notification from './Notification.jsx'
 import data from './data.jsx'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [notification, setNotification] = useState(null)
+  const [error, setError] = useState(null)
   const [personsShown, setPersonsShown] = useState([]) 
 
   useEffect(() => {
@@ -29,12 +31,21 @@ const App = () => {
           setPersons(persons.filter((person) => person.id != data.id))
           setPersonsShown(persons.filter((person) => person.id != data.id))
         })
+        .catch(error => {
+          if(error.status == 404) {
+            setError(`${name} has already been removed from the phonebook.`)
+            setTimeout(() => {
+              setError(null)
+            }, 5000)
+          }
+        })
     }
   }
   
   return (
     <div>
       <h2>Phonebook</h2>
+      <Error message={error}/>
       <Notification message={notification}/>
       <Filter 
         persons={persons}
@@ -53,7 +64,7 @@ const App = () => {
         setNotification = {setNotification}
       />  
       <h2>Numbers</h2>
-      <Person persons={personsShown} deleteEntree={deleteEntree}/>
+      <Person persons={personsShown} deleteEntree={deleteEntree} setError={setError}/>
     </div>
   )
 }
