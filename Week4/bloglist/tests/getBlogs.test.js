2928a081727ba32344c03b1bc9af5eeef67abbab
abdullah0x5c <1abdullahraja1@gmail.com>
2025-08-25 04:03:13 +0500

    4.9: Blog List Tests, step 2

--- Week4/bloglist/tests/getBlogs.test.js
+++ Week4/bloglist/tests/getBlogs.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, expect } = require('node:test')
 const assert = require('node:assert')
 const { initialBlog, blogsInDb } = require('./blogsHelper')
 const mongoose = require('mongoose')
@@ -9,6 +9,14 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
+const idAsKeyExists = (response) => {
+    response.body.forEach(obj => {
+        if(!('key' in obj)){
+            return 0
+        }
+    })
+}
+
 beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(initialBlog)
@@ -18,6 +26,12 @@ test('correct number of blogs are returned', async () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, initialBlog.length)
 })
+
+test('id as a property', async () => {
+    const response = await api.get('/api/blogs')
+    .expect(idAsKeyExists)
+})
+
 
 after( async () => {
     await mongoose.connection.close()
