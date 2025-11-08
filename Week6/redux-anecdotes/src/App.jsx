31a46ae25c8a781a17e5c29547dfa264332de9d2
abdullah0x5c <1abdullahraja1@gmail.com>
2025-11-08 23:40:59 +0500

    6.12 Anecdotes, step 10 && 6.13 Anecdotes, step 11

--- Week6/redux-anecdotes/src/App.jsx
+++ Week6/redux-anecdotes/src/App.jsx
@@ -1,12 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <br></br>
+      <Notification />
+      <br></br>
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- Week6/redux-anecdotes/src/store.jsx
+++ Week6/redux-anecdotes/src/store.jsx
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import reducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 export const store = configureStore({
   reducer: {
     anecdotes: reducer,
-    filter: filterReducer
+    filter: filterReducer,
+    notification: notificationReducer
   }
 })
